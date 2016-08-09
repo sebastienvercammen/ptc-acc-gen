e71a977875bdd48e05945f0e22f8c182627ba70d
pyr0ball <pyroballpcs@gmail.com>
2016-08-09 13:45:48 -0700

    Integrate xvfb to run script
    Handles ssh and headless environments

--- makeaccounts.sh
+++ makeaccounts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node index.js
+DEBUG=nightmare xvfb-run --server-args="-screen 0 1024x768x24" node index.js
 cd PogoPlayer
-node pogo.js
+DEBUG=nightmare xvfb-run --server-args="-screen 0 1024x768x24" node pogo.js
 echo "Find the generated accounts in PogoPlayer/accounts.csv"
