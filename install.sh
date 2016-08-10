7e36957f5a37836497dbb413fb652e20a590df3f
pyr0ball <pyroballpcs@gmail.com>
2016-08-09 17:20:09 -0700

    Updated to catch for non- Ubuntu installations
    Updated, will now install all dependencies, and will let the user know if install failed for some reason

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sudo apt-get install xvfb
+sudo apt-get update 2> /dev/null
+sudo apt-get install -y npm nodejs xvfb
+if [ $? -ne 0 ] ; then
+        echo depenencies install failed. Please use the installer of your choice to grab them
+        echo examples:
+        echo apt-get install xvfb npm nodejs
+        echo yum install xvfb xvfb npm nodejs
+fi
 sudo npm install
