c9486b0cd972a881cf9b6d443eccc8f8c8e36ea7
Patrick Parker <paddyloldev@gmail.com>
2016-08-13 18:39:14 +0100

    Added account names to debug info
    Added account names to debug info

--- PogoPlayer/pogo.js
+++ PogoPlayer/pogo.js
@@ -8,7 +8,7 @@ const acc = fs.readFileSync("./accounts.csv", "utf-8").split("\n").filter(x=>x!=
 for (let i = 0; i < acc.length; i++) {
   let a = acc[i];
   console.log(`Auth service: ${a[0]}, Username: ${a[1]}, Password: ${a[2]}, Location: ${a[3]},${a[4]}`);
-  
+
   let login = a[0] === "ptc"
     ? pogobuf.PTCLogin()
     : pogobuf.GoogleLogin();
@@ -21,11 +21,11 @@ for (let i = 0; i < acc.length; i++) {
   })
   .then(() => {
     let timeOut = Math.random()*3000+5000|0;
-    console.log(`Logged in. Doing stuff in ${timeOut/1000} seconds`);
+    console.log(`${a[1]} - Logged in. Doing stuff in ${timeOut/1000} seconds`);
     setTimeout(() => {
       new Promise((resolve) => {
         resolve(client.encounterTutorialComplete(1))
-      })    
+      })
       .then(() => {
         new Promise((resolve) => {
           resolve(client.markTutorialComplete(0, false, false));
@@ -44,8 +44,8 @@ for (let i = 0; i < acc.length; i++) {
 function pickTrainerName (client, a) {
   a[5] = a[5].replace(/\r/g, '');
   if (a[5].length > 12) {
-    console.log(`Seems like ${a[5]} is too long name... (PROTIP: It can't exceed 12 characters!) Run this to try different name!:
-node name.js -a ${a[0]} -u ${a[1]} -p ${a[2]} -l ${a[3]},${a[4]} -u TRAINER-NAME`); 
+    console.log(`${a[1]} - Seems like ${a[5]} is too long name... (PROTIP: It can't exceed 12 characters!) Run this to try different name!:
+node name.js -a ${a[0]} -u ${a[1]} -p ${a[2]} -l ${a[3]},${a[4]} -u TRAINER-NAME`);
   }
   new Promise(resolve=>{
     resolve(client.checkCodenameAvailable(a[5]));
@@ -57,7 +57,7 @@ node name.js -a ${a[0]} -u ${a[1]} -p ${a[2]} -l ${a[3]},${a[4]} -u TRAINER-NAME
         console.log(`Your trainer now is known as ${a[5]}!`);
       }).catch(err => {"Failed to set name... ERROR:", console.error(err);});
     } else {
-      console.log(`Seems like there's already someone known as ${a[5]}... Run this to try different name!:
+      console.log(`${a[1]} - Seems like there's already someone known as ${a[5]}... Run this to try different name!:
 node name.js -a ${a[0]} -u ${a[1]} -p ${a[2]} -l ${a[3]},${a[4]} -u TRAINER-NAME`);
     }
   }).catch(err => {console.error("Failed to check for name... ERROR:", err);});
