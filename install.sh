0143f84a838cbca7d050e577ba4e9be9f4918a0a
FrostTheFox <frost@walnutgaming.com>
2016-08-16 20:24:33 -0400

    remove xvfb

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update 2> /dev/null
-sudo apt-get install -y npm nodejs xvfb
+sudo apt-get install -y npm nodejs 
 if [ $? -ne 0 ] ; then
         echo depenencies install failed. Please use the installer of your choice to grab them
         echo examples:
-        echo apt-get install xvfb npm nodejs
-        echo yum install xvfb xvfb npm nodejs
+        echo apt-get install npm nodejs
+        echo yum install npm nodejs
 fi
 sudo npm install
