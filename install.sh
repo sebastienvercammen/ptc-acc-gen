ce8864a92ff43a87742472110d9c1417b76684d6
pyr0ball <pyroballpcs@gmail.com>
2016-08-17 17:50:03 -0700

    updated install.sh for linux users. catches git not being installed

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-git submodule init
-git submodule update
+which git 2>&1 >/dev/null
 if [ $? -ne 0 ] ; then
-        echo submodule install failed.
-        echo Maybe try cloning recursively?
-        echo git clone --recursive https://github.com/FrostTheFox/ptc-acc-gen.git
+        echo git is not currently installed
+        echo Please install git before proceeding
+	else
+		git submodule init
+		git submodule update
+	if [ $? -ne 0 ] ; then
+			echo submodule install failed.
+			echo Maybe try cloning recursively?
+			echo git clone --recursive https://github.com/FrostTheFox/ptc-acc-gen.git
+	fi
+		sudo apt-get update 2> /dev/null
+		sudo apt-get install -y npm nodejs
+	if [ $? -ne 0 ] ; then
+			echo depenencies install failed. Please use the installer of your choice to grab them
+			echo examples:
+			echo apt-get install npm nodejs
+			echo yum install npm nodejs
+		else
+			sudo npm install
+	fi
 fi
-sudo apt-get update 2> /dev/null
-sudo apt-get install -y npm nodejs 
-if [ $? -ne 0 ] ; then
-        echo depenencies install failed. Please use the installer of your choice to grab them
-        echo examples:
-        echo apt-get install npm nodejs
-        echo yum install npm nodejs
-fi
-sudo npm install
