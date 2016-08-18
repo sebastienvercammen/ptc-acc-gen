bb5343909e221e493b91763700ec53ab8f64991d
pyr0ball <pyroballpcs@gmail.com>
2016-08-17 21:32:43 -0700

    fixed windows syntax
    Please enter the commit message for your changes. Lines starting

--- install.bat
+++ install.bat
@@ -1,25 +1,25 @@
 @echo off
-git --version 2>&1 >NUL
-if %errorlevel% NEQ 0 (
+git --version
+if not "%errorlevel%" == "0" (
 	echo git is currently not installed, or has not been added to your system PATH
 	echo please install git, or if git is already installed, run:
 	echo setx PATH "%PATH%,C:\Program Files\Git\mingw64\libexec\git-core"
 	echo If using 32-bit or alternate install path, replace your git path in the above command
 	pause
 ) else (
-	git bash
-	git submodule init
-	git submodule update
-	if [ $? -ne 0 ] ; then
-			echo submodule install failed.
-			echo Maybe try cloning recursively?
-			echo git clone --recursive https://github.com/FrostTheFox/ptc-acc-gen.git
-	fi
-		
-		node --version 2>&1 >/dev/null
-	if [ $? -ne 0 ] ; then
-			echo NodeJS is not currently installed correctly. Please install that, then run "npm install" from your PokemonGo-Map folder.
-		else
-			npm install
+	git submodule init 2>&1 >NUL
+	git submodule update 2>&1 >NUL
+	if not "%errorlevel%" == "0" (
+		echo submodule install failed.
+		echo Maybe try cloning recursively?
+		echo git clone --recursive https://github.com/FrostTheFox/ptc-acc-gen.git
+	) else (	
+		node --version 2>&1 >NUL
+	)
+	if not "%errorlevel%" == "0" (
+		echo NodeJS is not currently installed correctly. Please install that, then run "npm install" from your PokemonGo-Map folder.
+	) else (
+		npm install
+	)
 	fi
 exit
