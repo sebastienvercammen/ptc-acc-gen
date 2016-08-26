184e522d63d83b179f995f5996957972702d5c8f
Frost The Fox <zx6r@walnutgaming.com>
2016-08-26 17:11:15 -0400

    revert pls

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
 	// Starts gen from startNumber to endNumber, determining how many accounts are made
 startNum:0,
-endNum:2,
+endNum:10,
 	
 	// Creation Options
 // Use nicknames file, or just append numbers to username?
@@ -15,17 +15,17 @@ screenshotOnFailure:true,
 	
 	// Creation Requirements
 // Keep the '', User- & display name. Make sure any "(username + number)@domain.com" is 100% unique, and is 6 characters minimum, but under 14 characters after the numbers are applied.
-username:"countryranq",
+username:"CHANGEME",
 // If you set randomPassword to 'false' above change this to your chosen password (so you have same password for all accounts)
-password:"NOSTATIC",
+password:"CHANGEME",
 // Enter your email address name. If your address is email@domain.com you'd enter 'email'
-emailUser:"frost",
+emailUser:"email",
 // Domain of email address. If your address is email@domain.com  you'd enter 'domain.com'
-emailDomain:"frostthefox.pw",
+emailDomain:"gmail.com",
 // Location Latitude for initial login
-latitude:"40.6706",
+latitude:"36.54596",
 // Location Longitude for initial login
-longitude:"78.2386",
+longitude:"-79.22247",
 // Country code (e.g. BE, FR, US, CA)
 country:"US"
 };
