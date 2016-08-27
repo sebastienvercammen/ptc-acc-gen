c553bc49cf98daf394961a55f5cf929d5aff916e
FrostTheFox <frost@walnutgaming.com>
2016-08-26 22:56:34 -0400

    wiping to fix everything. FUCK. YOU. GIT.

--- makeaccounts.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-node index.js
-cd PogoPlayer
-node pogo.js
-echo "Find the generated accounts in PogoPlayer/accounts.csv"
-pause
--- makeaccounts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# fuck headless systems, we got a captcha now
-node index.js
-cd PogoPlayer
-node pogo.js
-echo "Find the generated accounts in PogoPlayer/accounts.csv"
