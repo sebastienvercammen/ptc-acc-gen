01f871ad4282c582dbcb69dd9de88e53b4993d27
Sébastien Vercammen <contact@sebastienvercammen.be>
2016-12-16 20:34:59 +0100

    Removed forced debugging.

--- index.js
+++ index.js
@@ -314,13 +314,9 @@ function fillSignupPage(ctr) {
                                 .click('.button-green[value=" Continue"]')
                                 .then(function() {
                                     nightmare.wait(function() {
-                                            console.log("[DEBUG] Validating username in use.");
-
                                             return (document.getElementById("signup-signin") !== null || document.getElementById("btn-reset") !== null || document.body.textContent.indexOf("That username already exists") > -1);
                                         })
                                         .evaluate(function() {
-                                            console.log("[DEBUG] Validating success.");
-
                                             return (document.body.textContent.indexOf("Hello! Thank you for creating an account!") > -1);
                                         })
                                         .then(function(success) {
@@ -382,13 +378,9 @@ function fillSignupPage(ctr) {
             })
             .check("#id_terms")
             .wait(function() {
-                console.log("[DEBUG] Waiting for non-captcha signup page.");
-
                 return (document.getElementById("signup-signin") !== null || document.getElementById("btn-reset") !== null || document.body.textContent.indexOf("That username already exists") > -1);
             })
             .evaluate(function() {
-                console.log("[DEBUG] Validating non-captcha success.");
-
                 return (document.body.textContent.indexOf("Hello! Thank you for creating an account!") > -1);
             })
             .then(function(success) {
